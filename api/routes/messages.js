b33454869a96297f95c8a4b65cb1de88885aa103
Shardul Rasal <shardul@reachandteach.in>
2026-02-09 13:58:04 +0530

    fixed error

--- api/routes/messages.js
+++ api/routes/messages.js
@@ -1,5 +1,5 @@
 const redis = require("../redis")
-const { startSenderLoop } = require("../socketManager")
+const { startSenderLoop } = require("../worker/socketManager")
 
 module.exports = async function (fastify) {
   fastify.post("/messages/send", async (req, res) => {
