e55c40dcb5824a4927b9c12a72d6d35fdda9a3f2
Shardul Rasal <shardul@reachandteach.in>
2026-02-13 17:44:30 +0530

    code

--- api/routes/ws.js
+++ api/routes/ws.js
@@ -2,25 +2,20 @@ const { register } = require("../wsHub")
 const redis = require("../redis")
 
 module.exports = async function (fastify) {
-  fastify.get("/ws", { websocket: true }, (socket) => {
+  fastify.get("/ws", { websocket: true }, (socket, req) => {
 
-    let registeredClientId = null
+    let registered = false
 
     socket.on("message", async (raw) => {
       try {
         const { clientId } = JSON.parse(raw.toString())
         if (!clientId) return
 
-        // 🔒 Prevent double registration on multiple FE messages
-        // if (!registeredClientId) {
-        //   registeredClientId = clientId
-        //   register(clientId, socket)
-        // }
+        if (!registered) {
+          register(clientId, socket)
+          registered = true
+        }
 
-        registeredClientId = clientId
-        register(clientId, socket)
-
-        // 🔁 Always send current state
         const state = await redis.hget("wa:clients:state", clientId)
 
         socket.send(JSON.stringify({
@@ -29,7 +24,6 @@ module.exports = async function (fastify) {
           state: state || "NON_EXISTENT"
         }))
 
-        // 🔁 Always send stored QR (if exists)
         const qr = await redis.get(`wa:qr:${clientId}`)
         if (qr) {
           socket.send(JSON.stringify({
@@ -42,10 +36,6 @@ module.exports = async function (fastify) {
       } catch (err) {
         console.error("WS init error", err)
       }
-    })
-
-    socket.on("close", () => {
-      registeredClientId = null
     })
   })
 }
